ebf0c929d9c0cbb5c4797e5fb86a96c5cd6eecb2
hhowaitorozu <nrinsalwa@gmail.com>
2026-05-04 13:41:42 +0700

    ip-based rate limit user

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // buat throttlenya ke route api yach
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\DemoMemoController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/demo-memos', [DemoMemoController::class, 'index']);
 Route::post('/demo-memos', [DemoMemoController::class, 'store']);
 Route::delete('/demo-memos/{id}', [DemoMemoController::class, 'destroy']);
+Route::post('/demo-memos', [DemoMemoController::class, 'store'])
+    ->middleware('throttle:10,1');
